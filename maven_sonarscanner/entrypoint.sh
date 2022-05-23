b4b6fbd36faa236696f587e882573547f3e8681f
Pablo Collazo Collarte <pablo.collazo@torusware.com>
2022-05-23 20:22:20 +0200

    Introducción de los plugins Checkstyle y Spotbugs como medidas de calidad (#16)
    * feat: first aprox. for checkstyle execution
    
    * feat: separated in jobs scanners and web publication
    
    * feat: added separated job to retrieve input project
    
    * fix: typo
    
    * fix: typo
    
    * fix: typo
    
    * feat: testing workflow divided in jobs
    
    * feat: testing workflow divided in jobs
    
    * feat: compressing files between jobs
    
    * unzip with quiet mode
    
    * feat: verbose unzip
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * feat: deleted zip/unzip operations
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * feat: added mount dir
    
    * feat: added mount dir
    
    * feat: added mount dir
    
    * fix: dir typo
    
    * feat: copying checkstyle results to upload as artifact
    
    * feat: copying checkstyle results to upload as artifact
    
    * test
    
    * feat: changed parameters order of upload-artifact action
    
    * feat: updated upload step
    
    * feat: test
    
    * feat: testing sonarqube
    
    * feat: test
    
    * feat: restored -d option in docker compose
    
    * fix: branch name
    
    * fix: results relative path
    
    * fix: results relative path
    
    * fixc: fixed path to upload artifact
    
    * test: testing working directory content
    
    * fix: updated paths from web results
    
    * test: artifact upload
    
    * added spotbugs resolution
    
    * added -r option to copy command
    
    * feat: change to the correct project
    
    * feat: added spotbugs result to page
    
    * fix:name typo
    
    * fix: bash syntax
    
    * test: spotbugs report
    
    * test: spotbugs report
    
    * test: spotbugs report
    
    * test: spotbugs report
    
    * test: spotbugs report
    
    * test: spotbugs report
    
    * feat: build spot ugs report table
    
    * feat: added bugs as env var
    
    * fix: python executable path
    
    * feat: fIXED URL

--- maven_sonarscanner/entrypoint.sh
+++ maven_sonarscanner/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 /wait-for-it.sh sonarqube:9000 --strict -- \
-    sleep 60
+    sleep 65
     curl -X POST -u admin:admin -d "name=custom" http://sonarqube:9000/api/qualitygates/create
     curl -X POST -u admin:admin -d "name=custom" http://sonarqube:9000/api/qualitygates/set_as_default
     curl -X POST -u admin:admin -d "gateName=custom&metric=coverage&op=LT&error=80" http://sonarqube:9000/api/qualitygates/create_condition
@@ -9,4 +9,4 @@
     curl -X POST -u admin:admin -d "gateName=custom&metric=reliability_rating&op=GT&error=1" http://sonarqube:9000/api/qualitygates/create_condition
     curl -X POST -u admin:admin -d "gateName=custom&metric=security_rating&op=GT&error=1" http://sonarqube:9000/api/qualitygates/create_condition
     curl -X POST -u admin:admin -d "gateName=custom&metric=security_hotspots_reviewed&op=LT&error=100" http://sonarqube:9000/api/qualitygates/create_condition
-    mvn clean verify -DskipTests sonar:sonar -Dsonar.projectKey=project -Dsonar.host.url=http://sonarqube:9000 -Dsonar.scm.provider=git -Dsonar.login=admin -Dsonar.password=admin
+    mvn clean verify -DskipTests sonar:sonar -Dsonar.projectKey=project -Dsonar.host.url=http://sonarqube:9000 -Dsonar.login=admin -Dsonar.password=admin
---
+++ spotbugs/generate-spotbugs-results.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+from mdutils.mdutils import MdUtils
+from mdutils import Html
+import os
+
+
+
+class SpotbugsResults:
+
+  def generate_results(self):
+
+    bugs = os.getenv('bugs')
+
+    mdFile = MdUtils(file_name='bug_report', title='Spotbugs analysis results')
+    mdFile.new_header(1, "This GitHub page shows the spotbugs analysis results for the requested project")
+    list_of_strings = ["Nº", "Severity", "Description", "Bug"]
+
+    for y in range(bugs.count('[ERROR]')):
+      description = bugs.split('[ERROR] ')[y+1].split(':', 1)[1].split(' ')
+      bug = description.pop()
+      description = " ".join(description)
+      list_of_strings.extend([str(y), bugs.split('[ERROR] ')[y+1].split(':', 1)[0], description, bug])
+    mdFile.new_table(columns=4, rows=bugs.count('[ERROR]')+1, text=list_of_strings, text_align='center')
+
+    mdFile.create_md_file()
+    
+
+if __name__ == "__main__":
+    SpotbugsResults().generate_results()
